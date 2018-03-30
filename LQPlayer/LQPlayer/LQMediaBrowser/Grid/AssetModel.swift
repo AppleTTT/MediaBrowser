174e7ceb7d83e12b9d02e97cc1c3c8e0d8e8e3dd
李树 <at@lishudeMacBook-Pro.local>
2018-03-30 10:44:10 +0800

    [feature]1、 add some third lib；2、add video time label

---
+++ LQPlayer/LQPlayer/LQMediaBrowser/Grid/AssetModel.swift
@@ -0,0 +1,29 @@
+//
+//  GridCellModel.swift
+//  LQPlayer
+//
+//  Created by 李树 on 2018/3/27.
+//  Copyright © 2018年 laiqu. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+
+class AssetModel {
+    
+    var asset: PHAsset?
+    var isSelected: Bool = false
+    
+    init(asset: PHAsset, isSelected: Bool = false) {
+        self.asset = asset
+        self.isSelected = isSelected
+    }
+    
+}
+
+
+
+
+
+
---
+++ LQPlayer/Pods/Target Support Files/Pods-LQPlayerUITests/Pods-LQPlayerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LQPlayerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LQPlayerUITests
+@end
